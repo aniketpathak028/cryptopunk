7c593d2a6fed34551bbd1090c170919813e13271
Aniket Pathak <aniketpathak028@gmail.com>
2021-12-14 21:52:03 +0530

    fetching NFT data through opensea api

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import weth from "../assets/weth.png";
 import "./CollectionCard.css";
+import axios from "axios";
 
 function CollectionCard({ id, name, traits, img }) {
+    const [punkListData, setPunkListData] = useState([]);
+    
+    useEffect(() => {
+        const getMyNfts = async () => {
+            const openSeaData = await axios.get(
+                'https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x83A164Ad6947ddDc64Fe959e50625760b51946bB'
+            );
+            console.log(openSeaData.data.assets);
+            setPunkListData(openSeaData.data.assets);
+        };
+        getMyNfts();
+    }, []);
+
   return (
     <div className="collectionCard">
       <img src={img} alt="pic" />
